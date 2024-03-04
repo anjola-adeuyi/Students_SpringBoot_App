9de11ae3d20599e1a93e2e1ce9473529cabc5d0a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-03-04 09:57:53 +0100

    feat: API layer student controller

--- src/main/java/com/students/Application.java
+++ src/main/java/com/students/Application.java
@@ -1,34 +1,13 @@
 package com.students;
 
-import com.students.students.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Jola",
-						"Jola@gmail.com",
-						LocalDate.of(2000, Month.APRIL, 5),
-						20
-				)
-		);
 	}
 
 }
