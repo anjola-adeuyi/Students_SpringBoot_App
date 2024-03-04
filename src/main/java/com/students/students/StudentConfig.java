4aa8eb3cf90f0b993fab3601a388d4e46d8720da
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-03-05 00:39:40 +0100

    feat: Repository & Service database Config

---
+++ src/main/java/com/students/students/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.students.students;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository
+    ) {
+        return args -> {
+            Student jola = new Student (
+                    "Jola",
+                    "Jola@gmail.com",
+                    LocalDate.of(2000, Month.APRIL, 5),
+                    20
+            );
+
+            Student alex = new Student (
+                    "Alex",
+                    "Alex@gmail.com",
+                    LocalDate.of(1990, Month.DECEMBER, 5),
+                    20
+            );
+
+            repository.saveAll(
+                    List.of(jola, alex)
+            );
+        };
+    }
+}
